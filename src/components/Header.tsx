e0ad58c0dc091c91dac78f8f8fbc42d245738598
caiohmac <caiohmac@gmail.com>
2025-01-07 18:08:10 -0300

    Add Header and Empty icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css';
 export function Header() {
   return (
     <header className={styles.container}>
-      <img src="/logo.svg" alt="logo da aplicação" />
+      <img src="/Logo.svg" alt="logo da aplicação" />
     </header>
   )
 }
--- src/components/list/Empty.tsx
+++ src/components/list/Empty.tsx
@@ -3,6 +3,7 @@ import styles from './Empty.module.css';
 export function Empty() {
   return (
     <div className={styles.container}>
+      <img src="./Clipboard.svg" alt="Ícone de prancheta" />
         <p>
             <strong>Você ainda não tem tarefas cadastradas</strong>
             Crie tarefas e organize seus itens a fazer
